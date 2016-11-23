3e06aaf266f4bdc5679a79cc64ce488feba01a5e
daigo3 <stain.witness@gmail.com>
2016-11-23 15:35:15 +0900

    [add] step 3

--- source/grange.js
+++ source/grange.js
@@ -7,6 +7,14 @@ function* grange (start, end, transform, options) {
 
   const { step = 1 } = options || {};
 
+  if (start > end) {
+    for (let i = start; i >= end; i -= step) {
+      yield shouldTransform ? transform(i) : i;
+    }
+
+    return;
+  }
+
   for (let i = start; i <= end; i += step) {
     yield shouldTransform ? transform(i) : i;
   }
